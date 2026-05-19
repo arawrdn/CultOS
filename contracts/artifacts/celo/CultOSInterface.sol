627286d0a59e96bbba0a939c457c50aac8d3c2c4
arawrdn <aradeawardana97@gmail.com>
2026-05-19 12:51:52 +0000

    chore(cultos): auto-update Celo and Stacks interface bindings [skip ci]

--- contracts/artifacts/celo/CultOSInterface.sol
+++ contracts/artifacts/celo/CultOSInterface.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 // AUTO-GENERATED: CultOS Celo Bindings
-// Build Timestamp: Tue May 19 12:51:50 UTC 2026
-// Checksum: 2a2048a6ac0d
+// Build Timestamp: Tue May 19 12:51:52 UTC 2026
+// Checksum: 0e71e36e9144
--- contracts/artifacts/stacks/cultos-bindings.clar
+++ contracts/artifacts/stacks/cultos-bindings.clar
@@ -1,3 +1,3 @@
 ;; AUTO-GENERATED: CultOS Stacks Bindings
-;; Build Timestamp: Tue May 19 12:51:50 UTC 2026
-;; Checksum: 2a2048a6ac0d
+;; Build Timestamp: Tue May 19 12:51:52 UTC 2026
+;; Checksum: 0e71e36e9144
