e989dc8e216899140a3f01a0abea8fb6ba83ce70
arawrdn <aradeawardana97@gmail.com>
2026-05-20 00:06:32 +0000

    build(abi): compile latest CultOS core artifacts for network sync [skip ci]

--- contracts/artifacts/celo/CultOSInterface.sol
+++ contracts/artifacts/celo/CultOSInterface.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: MIT
 // AUTO-GENERATED: CultOS Celo Bindings
-// Build Timestamp: Wed May 20 00:06:30 UTC 2026
-// Checksum: d58c224b546d
+// Build Timestamp: Wed May 20 00:06:32 UTC 2026
+// Checksum: 435ffe97e649
